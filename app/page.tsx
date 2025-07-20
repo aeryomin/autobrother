e9278b7849533e6f08019d3b8e092b8261c5db6a
aeryomin <eryomin.developer@gmail.com>
2025-07-20 23:10:31 +0400

    refactored

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
-import Image from 'next/image'
-import Header from './components/Header/Header'
-import Hero from './components/Hero/Hero'
-import Services from './components/Services/Services'
-import Advantages from './components/Advantages/Advantages'
-import styles from './page.module.css'
+import Image from "next/image";
+import Header from "./components/Header/Header";
+import Hero from "./components/Hero/Hero";
+import Services from "./components/Services/Services";
+import Advantages from "./components/Advantages/Advantages";
+import styles from "./page.module.css";
 
 export default function Home() {
   return (
@@ -13,23 +13,5 @@ export default function Home() {
       <Services />
       <Advantages />
     </main>
-  )
+  );
 }
-
-const services = [
-  { icon: 'engine', title: 'Ремонт\nдвигателя' },
-  { icon: 'oil', title: 'Замена\nмасла' },
-  { icon: 'diagnostic', title: 'Компьютерная\nдиагностика' },
-  { icon: 'tire', title: 'Шиномонтаж' },
-  { icon: 'suspension', title: 'Ремонт\nподвески' },
-  { icon: 'alignment', title: 'Сход-развал' },
-  { icon: 'maintenance', title: 'Плановое ТО' },
-  { icon: 'filter', title: 'Замена\nфильтров' }
-]
-
-const advantages = [
-  { icon: 'masters', text: 'Опытные мастера с профильным образованием' },
-  { icon: 'price', text: 'Прозрачные цены и официальные документы' },
-  { icon: 'speed', text: 'Быстрое выполнение работ' },
-  { icon: 'guarantee', text: 'Гарантия на все виды услуг' }
-] 
